008dde53fa036c44238eb9dc3e20eb7483be899f
Njoki <kinuthiash@gmail.com>
2022-04-09 16:25:43 +0300

    add distance from HQ function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+
+//pick up point distance using if else
+
+function distanceFromHqInBlocks(block){
+  if(block > 42){
+    return block-42
+  }
+  else{
+    return 42-block
+  }  
+}
+
+// //pick up point distance using ternary
+// let distanceFromHqInBlocks = (block)=>block>42?block-42:42-block;
+
+console.log(distanceFromHqInBlocks(6))
