74e387f23e8ebeb6c8a9a2933beceff201b283a7
Njoki <kinuthiash@gmail.com>
2022-04-09 16:51:16 +0300

    add distance travelled in feet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+// //pick up point distance using ternary
+// let distanceFromHqInBlocks = (block)=>block>42?block-42:42-block;
 
 //pick up point distance using if else
-
 function distanceFromHqInBlocks(block){
   if(block > 42){
     return block-42
@@ -10,7 +11,29 @@ function distanceFromHqInBlocks(block){
   }  
 }
 
-// //pick up point distance using ternary
-// let distanceFromHqInBlocks = (block)=>block>42?block-42:42-block;
+let blocks = distanceFromHqInBlocks(43);
+console.log(blocks)
 
-console.log(distanceFromHqInBlocks(6))
+
+// Convert the blocks to feet 
+function distanceFromHqInFeet(block){
+  if(block > 42){
+    return (block-42) * 264
+  }
+  else{
+    return (42-block) * 264
+  } 
+}
+console.log(distanceFromHqInFeet(34))
+
+// distanceTravelledInFeet
+function distanceTravelledInFeet(block1,block2){
+  if(block1 > block2){
+    return (block1-block2) * 264
+  }
+  else{
+    return (block2-block1) * 264
+  } 
+}
+
+console.log(distanceTravelledInFeet(43,48))
